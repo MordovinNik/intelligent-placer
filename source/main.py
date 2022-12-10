eae2218d638fb383d02aad012d3d9943ea2bd214
MordovinNik <mordovin_nikolya@mail.ru>
2022-12-10 22:29:04 +0300

    Изменил план

--- source/main.py
+++ source/main.py
@@ -6,6 +6,6 @@ if __name__ == "__main__":
     background_path = "../objects/0.jpg"
     placer = Placer(background_path, dataset_path)
     placer.load_image("../tests/1.jpg")
-    print(placer.place_objects(1e-3))
+    print(placer.place_objects(1e-3, 1000))
     plt.imshow(placer.draw_objects())
     plt.show()
